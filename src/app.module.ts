679fb6351a9784b88a85704bf271a100d8802bcc
ZawNaingOo0 <zawnaingoo.mhs@gmail.com>
2023-12-14 21:57:14 +0630

    prismaSetup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { BookmerModule } from './bookmer/bookmer.module';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ AuthModule, BookmerModule, UsersModule],
+  imports: [ UsersModule, DatabaseModule],
 })
 export class AppModule {}
